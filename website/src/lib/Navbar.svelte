58c65ff882c230fcee8f8cae2b89c041dae9cdd2
Tadas Petra <tadas@tadaspetra.com>
2022-03-19 17:58:32 -0500

    actual basic template

---
+++ website/src/lib/Navbar.svelte
@@ -0,0 +1,11 @@
+<div class="navbar">
+	<div class="center">hungrimind</div>
+	<div class="center">feed your curiousity everyday</div>
+</div>
+
+<style>
+	.center {
+		display: flex;
+		justify-content: center;
+	}
+</style>
--- website/src/routes/index.svelte
+++ website/src/routes/index.svelte
@@ -1,59 +1,24 @@
-<script context="module">
-	export const prerender = true;
-</script>
-
 <script>
-	import Counter from '$lib/Counter.svelte';
+	function resizeIframe(obj) {
+		obj.style.height = obj.contentWindow.document.documentElement.scrollHeight + 'px';
+	}
 </script>
 
-<svelte:head>
-	<title>Home</title>
-</svelte:head>
-
-<section>
-	<h1>
-		<div class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</div>
-
-		to your new<br />SvelteKit app
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
-	<Counter />
-</section>
+<h2>Video Of The Day</h2>
+<iframe
+	title="Youtube Video Player"
+	width="100%"
+	height="600px"
+	src="https://www.youtube.com/embed/D5yTwnwklVE"
+	frameborder="0"
+	scrolling="no"
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+	allowfullscreen
+/>
 
 <style>
-	section {
+	h2 {
 		display: flex;
-		flex-direction: column;
 		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
 	}
 </style>
