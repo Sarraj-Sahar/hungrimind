1aa8588759ea2d970702b7447c6a739ab43ffeef
Tadas Petra <tadas@tadaspetra.com>
2022-06-09 13:58:39 -0500

    Removed themeprovider in general

--- pages/_app.js
+++ pages/_app.js
@@ -3,10 +3,8 @@ import { ClientReload } from '@/components/ClientReload'
 import LayoutWrapper from '@/components/LayoutWrapper'
 import '@/css/prism.css'
 import '@/css/tailwind.css'
-import siteMetadata from '@/data/siteMetadata'
 import '@fontsource/inter/variable-full.css'
 import 'katex/dist/katex.css'
-import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
 
@@ -17,7 +15,7 @@ const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" theme={siteMetadata.theme}>
+    <div>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
@@ -26,6 +24,6 @@ export default function App({ Component, pageProps }) {
       <LayoutWrapper>
         <Component {...pageProps} />
       </LayoutWrapper>
-    </ThemeProvider>
+    </div>
   )
 }
