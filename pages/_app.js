c94fb08c1944d2190855aa74a27d71be0df6d55d
Tadas Petra <tadas@tadaspetra.com>
2022-06-09 09:42:11 -0500

    force light theme

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,23 @@
-import '@/css/tailwind.css'
+import Analytics from '@/components/analytics'
+import { ClientReload } from '@/components/ClientReload'
+import LayoutWrapper from '@/components/LayoutWrapper'
 import '@/css/prism.css'
-import 'katex/dist/katex.css'
-
+import '@/css/tailwind.css'
+import siteMetadata from '@/data/siteMetadata'
 import '@fontsource/inter/variable-full.css'
-
+import 'katex/dist/katex.css'
 import { ThemeProvider } from 'next-themes'
 import Head from 'next/head'
 
-import siteMetadata from '@/data/siteMetadata'
-import Analytics from '@/components/analytics'
-import LayoutWrapper from '@/components/LayoutWrapper'
-import { ClientReload } from '@/components/ClientReload'
+
+
 
 const isDevelopment = process.env.NODE_ENV === 'development'
 const isSocket = process.env.SOCKET
 
 export default function App({ Component, pageProps }) {
   return (
-    <ThemeProvider attribute="class" defaultTheme={siteMetadata.theme}>
+    <ThemeProvider attribute="class" forcedTheme={siteMetadata.theme}>
       <Head>
         <meta content="width=device-width, initial-scale=1" name="viewport" />
       </Head>
