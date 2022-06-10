16ff57c9a0d4727514807ec2d7417fb3d043dfce
Tadas Petra <tadas@tadaspetra.com>
2022-06-10 18:58:42 -0500

    Update siteMetadata.js

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -19,7 +19,7 @@ const siteMetadata = {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: 'https://plausible.io/hungrimind.com', // e.g. tailwind-nextjs-starter-blog.vercel.app
+    plausibleDataDomain: 'hungrimind.com', // e.g. tailwind-nextjs-starter-blog.vercel.app
   },
   newsletter: {
     // supports mailchimp, buttondown, convertkit, klaviyo, revue, emailoctopus
