dd29993e622e12865e35c467c582e271a39b5d5a
Tadas Petra <tadas@tadaspetra.com>
2022-06-10 18:51:57 -0500

    analytics

--- next.config.js
+++ next.config.js
@@ -4,8 +4,8 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 // You might need to insert additional domains in script-src if you are using external services
 const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app *.plausible.io;
+  default-src 'self' plausible.io;
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
   media-src 'none';
