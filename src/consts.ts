0ad53292c2d940e5ef70f9d328767ef6deb786c1
Tadas Petra <tadas@tadaspetra.com>
2023-02-06 11:02:46 -0600

    doc: new content alert!!!

--- src/consts.ts
+++ src/consts.ts
@@ -44,6 +44,7 @@ export const SIDEBAR: Sidebar = {
 		'Learn': [
 			{ text: 'Widget Tree', link: 'flutter/widget_tree' },
 			{ text: 'Hot Reload & Hot Restart', link: 'flutter/hot_r' },
+			{ text: 'BuildContext', link: 'flutter/buildcontext' },
 		],
 		'Build': [
 			{ text: 'Coming Soon', link: 'flutter/comingsoon' },
